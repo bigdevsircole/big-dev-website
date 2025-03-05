b5d2d5bcede4cbffac19690b673e7db90d6d1b65
BigDev <email>
2025-03-05 16:48:25 +0100

    Update context.jsx

--- src/context.jsx
+++ src/context.jsx
@@ -1,21 +1,27 @@
-import { useContext } from "react";
-import { useState } from "react";
-import { createContext } from "react";
+import PropTypes from "prop-types";
+import { useContext, useState, createContext } from "react";
 
 const GlobalContext = createContext();
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const useGlobalContext = () => useContext(GlobalContext);
 
 const AppContext = ({ children }) => {
   const [count, setCount] = useState(0);
+
   const handleIncrease = () => {
     setCount((c) => c + 1);
   };
+
   return (
     <GlobalContext.Provider value={{ count, setCount, handleIncrease }}>
       {children}
     </GlobalContext.Provider>
   );
+};
+
+AppContext.propTypes = {
+  children: PropTypes.node.isRequired,
 };
 
 export default AppContext;
